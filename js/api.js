c339c3ff2b8e1de118991ebf4f36846db7d167b4
AR-Fahad <ashfaqurrahmanfahad@gmail.com>
2023-09-02 18:35:09 +0600

    last commit

--- js/api.js
+++ js/api.js
@@ -1,9 +1,10 @@
+let forSort;
 async function loadData(id) {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/videos/category/${id}`
   );
   const data = await res.json();
-  console.log(data);
+  forSort = id;
   const sec = document.getElementById("card-sec");
   sec.classList = "p-10 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4";
   sec.innerHTML = "";
@@ -11,7 +12,8 @@ async function loadData(id) {
     const div = document.createElement("div");
     div.classList = "card card-compact bg-gray-100 shadow-xl";
     div.innerHTML = `
-                <figure><img class="w-[300px] h-[200px]" src="${element.thumbnail}" alt="Shoes" /></figure>
+                <figure><img class="w-[335px] h-[200px]" src="${element.thumbnail}" alt="Shoes" /></figure>
+                <p id="${element.others.posted_date}" class=""></p>
                 <div class="card-body">
                     <div class="flex gap-3">
                         <div><img class="rounded-full w-10 h-10" src="${element.authors[0].profile_picture}" alt=""></div>
@@ -31,6 +33,18 @@ async function loadData(id) {
         <p class="inline">${element.authors[0].profile_name}</p>
         <img id class="inline" src="images/verified.png" alt="">
         `;
+    }
+
+    if (element.others.posted_date && element.others.posted_date <= 86400) {
+      let hours = element.others.posted_date / 3600;
+      hours = parseInt(hours);
+      let mins = element.others.posted_date / 60 - hours * 60;
+      mins = parseInt(mins);
+      document.getElementById(element.others.posted_date).classList =
+        "pl-1 bg-[#171717] text-white rounded-md relative w-[132px] h-6 bottom-8";
+      document.getElementById(
+        element.others.posted_date
+      ).innerText = `${hours}hrs ${mins}mins ago`;
     }
   });
 }
@@ -93,4 +107,61 @@ function btnAll() {
   document.getElementById("btn-drawing").classList =
     "btn btn-sm bg-gray-300 text-gray-700 border-none rounded";
   loadData("1000");
+}
+
+async function loadSortData(id) {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${id}`
+  );
+  const data = await res.json();
+  const sortData = data.data
+    .sort((a, b) => parseFloat(a.others.views) - parseFloat(b.others.views))
+    .reverse();
+  console.log(sortData);
+  const sec = document.getElementById("card-sec");
+  sec.classList = "p-10 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4";
+  sec.innerHTML = "";
+  sortData.forEach((element) => {
+    const div = document.createElement("div");
+    div.classList = "card card-compact bg-gray-100 shadow-xl";
+    div.innerHTML = `
+                <figure><img class="w-[335px] h-[200px]" src="${element.thumbnail}" alt="Shoes" /></figure>
+                <p id="${element.others.posted_date}" class=""></p>
+                <div class="card-body">
+                    <div class="flex gap-3">
+                        <div><img class="rounded-full w-10 h-10" src="${element.authors[0].profile_picture}" alt=""></div>
+                        <h2 class="card-title text-black font-bold">${element.title}</h2>
+                    </div>
+                    <div class="pl-12 text-gray-600">
+                        <div id="${element.title}">
+                            <p class="inline">${element.authors[0].profile_name}</p>
+                        </div>
+                        <p>${element.others.views} views</p>
+                    </div>
+                </div>
+    `;
+    sec.appendChild(div);
+    if (element.authors[0].verified) {
+      document.getElementById(element.title).innerHTML = `
+        <p class="inline">${element.authors[0].profile_name}</p>
+        <img id class="inline" src="images/verified.png" alt="">
+        `;
+    }
+
+    if (element.others.posted_date && element.others.posted_date <= 86400) {
+      let hours = element.others.posted_date / 3600;
+      hours = parseInt(hours);
+      let mins = element.others.posted_date / 60 - hours * 60;
+      mins = parseInt(mins);
+      document.getElementById(element.others.posted_date).classList =
+        "pl-1 bg-[#171717] text-white rounded-md relative w-[132px] h-6 bottom-8";
+      document.getElementById(
+        element.others.posted_date
+      ).innerText = `${hours}hrs ${mins}mins ago`;
+    }
+  });
+}
+
+function btnSort() {
+  loadSortData(forSort);
 }
